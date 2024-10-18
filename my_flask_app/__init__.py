1bd94f1f1d8e3dd3f302968ac43c231c8224b7ca
kimwook123 <rladnr1467@naver.com>
2024-10-18 17:06:16 +0900

    챗봇 대화 연동

--- my_flask_app/__init__.py
+++ my_flask_app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
-
+from flask_cors import CORS
 import config
 
 db = SQLAlchemy()
@@ -11,6 +11,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+    CORS(app)
 
     # ORM 초기화
     db.init_app(app)
@@ -18,11 +19,12 @@ def create_app():
     from . import models
 
     # 블루프린트 등록
-    from .views import main_views, question_views, answer_views, auth_views
+    from .views import main_views, question_views, answer_views, auth_views, text_chatbot
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     app.register_blueprint(auth_views.bp)
+    app.register_blueprint(text_chatbot.bp)
     
     # 필터
     from .filter import format_datetime
---
+++ my_flask_app/views/text_chatbot.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+from flask import Blueprint, url_for, request, render_template, g, flash, jsonify
+from werkzeug.utils import redirect
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langchain_core.messages import AIMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import ToolNode, tools_condition
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bp = Blueprint('chat', __name__, url_prefix='/chatbot')
+
+@bp.route('/chat', methods=['GET'])
+def chat_page():
+    return render_template('chatbot/text_chatbot.html')
+
+@bp.route('/chat', methods=['POST'])
+def chat():
+    try:
+        model_id = os.getenv('MODEL_ID')
+        model = ChatModel(model_id=model_id)
+        user_input = request.json.get('message')
+        response = model.get_response(user_input)
+
+        return jsonify({'response': list(response)})
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': str(e)}), 500
+    
+class ChatModel:
+    """Chat 모델 클래스는 주어진 모델 ID로 대화 모델을 초기화하고 요청에 대한 응답을 제공합니다."""
+    
+    def __init__(self, model_id):
+        api_key = os.getenv('OPENAI_API_KEY')
+        self.chat_model = ChatOpenAI(model=model_id)
+        self.tool = TavilySearchResults(max_results=3)
+        self.tools = [self.tool]
+        self.tool_node = ToolNode(tools=self.tools)
+        self.chat_model_with_tools = self.chat_model.bind_tools(self.tools)
+        self.memory = MemorySaver()
+        self.graph = self.graph_builder.compile(checkpointer=self.memory)
+        self.config = {'configurable': {'thread_id': '1'}}
+
+    def _call_model(self, state: MessagesState):
+        return {'messages': self.chat_model_with_tools.invoke(state['messages'])}
+
+    def get_response(self, prompt: str):
+        """모델에 대한 요청을 보내고 응답을 받는다."""
+        for chunk, metadata in self.graph.stream(
+                {'messages':prompt},
+                config=self.config,
+                stream_mode='messages'
+            ):
+            print(chunk)
+            if isinstance(chunk, AIMessage):
+                if isinstance(chunk.content, str):
+                    yield chunk.content
+                elif len(chunk.content) > 0 and \
+                    chunk.content[0]['type'] == 'text':
+                    yield chunk.content[0]['text']
