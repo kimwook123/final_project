2c7350f11a1056a374bcb30a1edaad4dd5ae1d26
kimwook123 <rladnr1467@naver.com>
2024-10-24 08:54:11 +0900

    이미지챗봇 주석추가

--- my_flask_app/views/image_chatbot.py
+++ my_flask_app/views/image_chatbot.py
@@ -1,3 +1,10 @@
+# 아직 미구현
+# 이미지 챗봇의 메커니즘은 요청 메뉴얼을 제작하여 AI에게 제공하면,
+# AI가 짜여진 프롬프트 메뉴얼에 따라 이미지를 생성하되,
+# Tavily 서칭을 활용한 구글링을 통해 최신 트렌드 키워드를 탐색하고,
+# 사용자가 제시한 키워드와 트렌드 키워드를 병합하여 이미지를 생성한다.
+# 따라서 구글링 후 생성 메커니즘이 텍스트 챗봇 작동방식과 비슷하다.
+
 from datetime import datetime
 from flask import Blueprint, url_for, request, render_template, g, flash, jsonify
 from werkzeug.utils import redirect
@@ -10,37 +17,36 @@ from langgraph.prebuilt import ToolNode, tools_condition
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # .env에 작성한 변수를 불러온다.
+load_dotenv()
 
-bp = Blueprint('chat', __name__, url_prefix='/chatbot')
+bp = Blueprint('image', __name__, url_prefix='/imagebot')
 
-@bp.route('/chat', methods=['GET'])
-def chat_page():
-    return render_template('chatbot/text_chatbot.html')
+@bp.route('/image', methods=['GET'])
+def image_chat_page():
+    return render_template('chatbot/image_chatbot.html')
 
-@bp.route('/chat', methods=['POST'])
-def chat():
+@bp.route('/image', methods=['POST'])
+def image_initialize():
     try:
-        model_id = os.getenv('MODEL_ID')
-        model = ChatModel(model_id=model_id)
+        model_id = os.getenv('IMAGE_MODEL_ID')
+        model = ImageChatModel(model_id=model_id)
         user_input = request.json.get('message')
         response = model.get_response(user_input)
 
         return jsonify({'response': list(response)})
+    
     except Exception as e:
         print(str(e))
         return jsonify({'error': str(e)}), 500
     
-class ChatModel:
-    """Chat 모델 클래스는 주어진 모델 ID로 대화 모델을 초기화하고 요청에 대한 응답을 제공합니다."""
-    
+class ImageChatModel:
     def __init__(self, model_id):
         api_key = os.getenv('OPENAI_API_KEY')
-        self.chat_model = ChatOpenAI(model=model_id)
+        self.imagechat_model = ChatOpenAI(model=model_id)
         self.tool = TavilySearchResults(max_results=3)
         self.tools = [self.tool]
         self.tool_node = ToolNode(tools=self.tools)
-        self.chat_model_with_tools = self.chat_model.bind_tools(self.tools)
+        self.imagechat_model_with_tools = self.imagechat_model.bind_tools(self.tools)
         self.graph_builder = StateGraph(MessagesState)
         self.graph_builder.add_node('model', self._call_model)
         self.graph_builder.add_node('tools', self.tool_node)
@@ -55,10 +61,9 @@ class ChatModel:
         self.config = {'configurable': {'thread_id': '1'}}
 
     def _call_model(self, state: MessagesState):
-        return {'messages': self.chat_model_with_tools.invoke(state['messages'])}
-
+        return {'messages': self.imagechat_model_with_tools.invoke(state['messages'])}
+    
     def get_response(self, prompt: str):
-        """모델에 대한 요청을 보내고 응답을 받는다."""
         try:
             response = self.graph.invoke(
                 {'messages': prompt},
