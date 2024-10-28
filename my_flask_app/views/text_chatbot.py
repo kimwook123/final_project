d9425a7fc963c4cdbbbed9415b3267fa092d7187
sdmalml <woqja2664@naver.com>
2024-10-28 15:04:35 +0900

    텍스트 챗봇 프롬프트 설정.

--- my_flask_app/views/text_chatbot.py
+++ my_flask_app/views/text_chatbot.py
@@ -33,7 +33,7 @@ def chat():
     
 class ChatModel:
     """Chat 모델 클래스는 주어진 모델 ID로 대화 모델을 초기화하고 요청에 대한 응답을 제공합니다."""
-    
+
     def __init__(self, model_id):
         api_key = os.getenv('OPENAI_API_KEY')
         self.chat_model = ChatOpenAI(model=model_id)
@@ -45,25 +45,24 @@ class ChatModel:
         self.graph_builder.add_node('model', self._call_model)
         self.graph_builder.add_node('tools', self.tool_node)
         self.graph_builder.set_entry_point('model')
-        self.graph_builder.add_conditional_edges(
-            'model',
-            tools_condition
-        )
+        self.graph_builder.add_conditional_edges('model', tools_condition)
         self.graph_builder.add_edge('tools', 'model')
         self.memory = MemorySaver()
         self.graph = self.graph_builder.compile(checkpointer=self.memory)
         self.config = {'configurable': {'thread_id': '1'}}
+        
+        # 프롬프트 메시지 추가
+        self.system_prompt = {"role": "system", "content": "너는 텍스트 광고를 만드는 챗봇이야. 텍스트 광고만 생성해야 해. 광고와 무관한 질문에는 '죄송합니다, 저는 광고 작성에만 도움을 드릴 수 있습니다.'라고 답하세요."}
 
     def _call_model(self, state: MessagesState):
-        return {'messages': self.chat_model_with_tools.invoke(state['messages'])}
+        # 시스템 프롬프트를 메시지의 시작에 추가
+        messages_with_prompt = [self.system_prompt] + state['messages']
+        return {'messages': self.chat_model_with_tools.invoke(messages_with_prompt)}
 
     def get_response(self, prompt: str):
         """모델에 대한 요청을 보내고 응답을 받는다."""
         try:
-            response = self.graph.invoke(
-                {'messages': prompt},
-                config=self.config
-            )
+            response = self.graph.invoke({'messages': prompt}, config=self.config)
 
             for message in response['messages']:
                 if isinstance(message, AIMessage):
