332f0e6f1127cc26f4887dd81f97acbcdadf5c80
kimwook123 <rladnr1467@naver.com>
2024-11-04 17:42:38 +0900

    텍스트 생성 시 db저장 구현

--- my_flask_app/views/auth_views.py
+++ my_flask_app/views/auth_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template, flash, request, session, g
+from flask import Blueprint, url_for, render_template, flash, request, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
